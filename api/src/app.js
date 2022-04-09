e089af39ddcd9603fe4aeb905e4fbfb6d9d353fe
Carlos Palomo Serna <cepalomos@outlook.es>
2022-04-09 09:14:49 -0500

    modelos terminados

--- api/src/app.js
+++ api/src/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
@@ -12,7 +11,6 @@ server.name = 'API';
 
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
-server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
