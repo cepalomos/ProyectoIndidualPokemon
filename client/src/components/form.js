9e9210b8c99e78d1913a8a480811eba4d7bdf4e1
Carlos Palomo Serna <cepalomos@outlook.es>
2022-04-24 11:11:33 -0500

    validaciones del formulario terminado

--- client/src/components/form.js
+++ client/src/components/form.js
@@ -1,31 +1,43 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import '../css/form.css';
+import validation from '../functions/validacion';
 
 export default function Form() {
   const [type,setType] = useState([]);
   const [input,setInput] = useState({});
   const [typeForm,setTypeForm] = useState([]);
+  const [error,setError] = useState({});
   useEffect(()=>{
     fetch("http://localhost:3001/types").then(res=>res.json()).then(json=>setType(json));
   },[])
+  useEffect(()=>{
+    validation(input,setError,error);
+  },[input]);
   return (
-    <form className='form_contenedor'>
+    <form className='form_contenedor' method='post' action=''>
       <legend className='form_titulo'>Crear Pokemon</legend>
       <label className='form_label' htmlFor='nombre'>Nombre</label>
       <input className='form_input' id='nombre' type="text" onChange={({target:{value}})=>setInput({...input,nombre:value})} value={input.nombre}></input>
+      <p className='error_name'>{error?.name}</p>
       <label className='form_label' htmlFor='vida'>Vida</label>
       <input className='form_input' type="number" id="vida" onChange={({target:{value}})=>setInput({...input,vida:value})} value={input.vida}/>
+      <p className='error_vida'>{error?.vida}</p>
       <label className='form_label' htmlFor='fuerza'>Fuerza</label>
       <input className='form_input' type="number" id="fuerza" onChange={({target:{value}})=>setInput({...input,fuerza:value})} value={input.fuerza}/>
+      <p className='error_fuerza'>{error?.fuerza}</p>
       <label className='form_label' htmlFor='defensa'>Defensa</label>
       <input className='form_input' type="number" id="defensa" onChange={({target:{value}})=>setInput({...input,defensa:value})} value={input.defensa}/>
+      <p className='error_defensa'>{error?.defensa}</p>
       <label className='form_label' htmlFor='velocidad'>Velocidad</label>
       <input className='form_input' type="number" id="velocidad" onChange={({target:{value}})=>setInput({...input,velocidad:value})} value={input.velocidad}/>
+      <p className='error_defensa'>{error?.velocidad}</p>
       <label className='form_label' htmlFor='altura'>Altura</label>
       <input className='form_input' type="number" id="altura" onChange={({target:{value}})=>setInput({...input,altura:value})} value={input.altura}/>
+      <p className='error_defensa'>{error?.altura}</p>
       <label className='form_label' htmlFor='peso'>Peso</label>
       <input className='form_input' type="number" id="peso" onChange={({target:{value}})=>setInput({...input,peso:value})} value={input.peso}/>
+      <p className='error_defensa'>{error?.peso}</p>
       <select className='form_select' onChange={({target:{value}})=>{
         if(typeForm.length<=1&&!typeForm.includes(value))
         setTypeForm([...typeForm,value])}}>
